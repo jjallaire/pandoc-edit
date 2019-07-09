c278c46bf99a16e39d691b4845b2693ea4faf9cd
jjallaire <jj@rstudio.com>
2019-07-09 13:17:09 -0400

    add eslint to project

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import PandocEditor from './editor'
 
 let place = document.getElementById('pandoc-editor');
 
-let editor = new PandocEditor({
-  place
-});
+new PandocEditor( { place });
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,18 @@ module.exports = {
         port: 8080
     },
     module: {
-        rules: [{
-            test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: "babel-loader" 
-        }]
+      rules: [
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "eslint-loader"
+        },
+        {
+          test: /\.js$/, 
+          exclude: /node_modules/, 
+          loader: "babel-loader"
+        }
+      ]
     }
 };
