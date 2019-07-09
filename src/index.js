dd794bcda5ffa87e0bf962146a8179db8c5d7711
jjallaire <jj@rstudio.com>
2019-07-09 10:25:25 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function greet(guestName) {
+    console.log(`Hello ${guestName}!`);
+}
+
+console.log("[INFO2] Script loaded...")
+
+greet('Guest');
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8080
+    },
+    module: {
+        rules: [{
+            test: /\.js$/, 
+            exclude: /node_modules/, 
+            loader: "babel-loader" 
+        }]
+    }
+};
