1536cfecf62328dbdf7039dbe876256f7852e62d
jjallaire <jj@rstudio.com>
2019-07-10 11:41:25 -0400

    call setContent

--- example/editor.js
+++ example/editor.js
@@ -1,8 +1,10 @@
 
 
 let place = document.getElementById('pandoc-editor');
+let editor = new PandocEditor.Editor( { place });
+
 let content = "**here** we go _again_ And again and again";
-let editor = new PandocEditor.Editor( { place, content });
+editor.setContent(content);
 
 document.getElementById('pandoc-get-content').addEventListener('click', function() {
   let output = document.getElementById('pandoc-markdown');
