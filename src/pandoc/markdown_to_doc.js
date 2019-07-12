4ed889c2d3bfc9b8c792924ccbb25ef1138b2340
jjallaire <jj@rstudio.com>
2019-07-12 09:58:24 -0400

    more cleanup of code

--- src/pandoc/markdown_to_doc.js
+++ src/pandoc/markdown_to_doc.js
@@ -11,86 +11,104 @@ import { Mark } from "prosemirror-model"
 // pandoc markdown-it processor:
 //  https://github.com/ProseMirror/prosemirror-markdown/blob/master/src/from_markdown.js
 
+// TOOD: error handling
 // TODO: more things dyanamic based on presence in schema
-// TODO: pass the schema through to handlers
 
 
 export function pandocMarkdownToDoc(markdown) {
-
-  return pandocMarkdown2Ast(markdown)
-    .then(ast => {
-      
-      // create handlers that map between pandoc AST and prosemirror doc
-      let handlers = tokenHandlers({
-        "Para": { block: "paragraph" },
-        "Emph": { mark: "em" },
-        "Strong": { mark: "strong" },
-        "Link": { mark: "link", 
-          getAttrs: tok => ({
-            href: tok.c[2][0],
-            title: tok.c[2][1] || null
-          }),
-          getChildren: tok => tok.c[1]
-        },
-        "Str": { text: true, getText: tok => tok.c },
-        "Space": { text: true, getText: () => " "}
-      });
-
-      // create object to track current state of conversion 
-      // (stack of block level nodes, active marks, etc.)
-      let state = new ConversionState(handlers);
-
-      
-
-
-      parseTokens(state, ast.blocks, handlers);
-    
-      return state.topNode();
-    });
-
+  return pandocMarkdownToAst(markdown)
+    .then(pandocAstToDoc);
 }
 
-// call backend pandoc handler to convert markdown into 
-// a JSON version of the pandoc AST
-function pandocMarkdown2Ast(markdown) {
+function pandocMarkdownToAst(markdown) {
   return axios.post("/pandoc/ast", { format: 'commonmark', markdown })
     .then(result => {
       return result.data.ast;
     })
 }
 
-function parseTokens(state, tokens, handlers) {
-  for (let tok of tokens) {
-    let handler = handlers[tok.t];
-    handler(state, tok);
+function pandocAstToDoc(ast) {
+  let parser = new PandocParser(pandocSchema, pandocTokenSpecs);
+  return parser.parse(ast);
+}
+
+let pandocTokenSpecs = {
+  "Para": { block: "paragraph" },
+  "Emph": { mark: "em" },
+  "Strong": { mark: "strong" },
+  "Link": { mark: "link", 
+    getAttrs: tok => ({
+      href: tok.c[2][0],
+      title: tok.c[2][1] || null
+    }),
+    getChildren: tok => tok.c[1]
+  },
+  "Str": { text: true, getText: tok => tok.c },
+  "Space": { text: true, getText: () => " "}
+};
+
+class PandocParser {
+
+  constructor(schema, tokenSpecs) {
+    this._schema = schema;
+    this._handlers = this._parseHandlers(tokenSpecs);
+  }
+
+  parse(ast) {
+    let state = new PandocParserState(this._schema);
+    this._parseTokens(state, ast.blocks);
+    return state.topNode();
+  }
+
+  _parseTokens(state, tokens) {
+    for (let tok of tokens) {
+      let handler = this._handlers[tok.t];
+      handler(state, tok);
+    }
+  }
+
+  _parseHandlers(tokenSpecs) {
+    let handlers = Object.create(null);
+    for (let type in tokenSpecs) {
+      let spec = tokenSpecs[type];
+      let getChildren = spec.getChildren || (tok => tok.c);
+      let getAttrs = spec.getAttrs || (() => {});
+      if (spec.text) {
+        handlers[type] = (state, tok) => {
+          let text = spec.getText(tok);
+          state.addText(text);
+        }
+      } else if (spec.mark) {
+        handlers[type] = (state, tok) => {
+          let markType = this._schema.marks[spec.mark];
+          let mark = markType.create(getAttrs(tok));
+          state.openMark(mark);
+          this._parseTokens(state, getChildren(tok));
+          state.closeMark(mark);
+        } 
+      } else if (spec.block) {
+        let nodeType = this._schema.nodeType(spec.block);
+        handlers[type] = (state, tok) => {
+          state.openNode(nodeType, getAttrs(tok));
+          this._parseTokens(state, getChildren(tok));
+          state.closeNode();
+        };
+      }
+    }
+    return handlers;
   }
 }
 
+class PandocParserState {
 
-class ConversionState {
-
-  constructor(tokenHandlers) {
-
-    // save references to schema and token handlers
-    this._schema = pandocSchema;
-    this._tokenHandlers = tokenHandlers;
-
-    // initialize state
+  constructor(schema) {
+    this._schema = schema;
     this._stack = [{type: this._schema.topNodeType, content: []}]
     this._marks = Mark.none    
   }
 
-  top() {
-    return this._stack[this._stack.length - 1]
-  }
-
   topNode() {
-    return this.top().type.createAndFill(null, this.top().content);
-  }
-
-  push(elt) {
-    if (this._stack.length) 
-      this.top().content.push(elt)
+    return this._top().type.createAndFill(null, this._top().content);
   }
 
   // Adds the given text to the current position in the document,
@@ -98,18 +116,13 @@ class ConversionState {
   addText(text) {
     if (!text) 
       return
-    let nodes = this.top().content
+    let nodes = this._top().content
     let last = nodes[nodes.length - 1]
     let node = this._schema.text(text, this._marks), merged
-    if (last && (merged = this.maybeMerge(last, node))) 
+    if (last && (merged = this._maybeMerge(last, node))) 
       nodes[nodes.length - 1] = merged
     else 
       nodes.push(node)
-  }
-
-  maybeMerge(a, b) {
-    if (a.isText && b.isText && Mark.sameSet(a.marks, b.marks))
-      return a.withText(a.text + b.text)
   }
 
   // : (Mark)
@@ -124,16 +137,6 @@ class ConversionState {
     this._marks = mark.removeFromSet(this._marks)
   }
 
-  // : (NodeType, ?Object, ?[Node]) → ?Node
-  // Add a node at the current position.
-  addNode(type, attrs, content) {
-    let node = type.createAndFill(attrs, content, this._marks)
-    if (!node) 
-      return null
-    this.push(node)
-    return node
-  }
-
   // : (NodeType, ?Object)
   // Wrap subsequent content in a node of the given type.
   openNode(type, attrs) {
@@ -146,39 +149,30 @@ class ConversionState {
     if (this._marks.length) 
       this._marks = Mark.none
     let info = this._stack.pop()
-    return this.addNode(info.type, info.attrs, info.content)
+    return this._addNode(info.type, info.attrs, info.content)
+  }
+
+
+  _top() {
+    return this._stack[this._stack.length - 1]
+  }
+
+  // : (NodeType, ?Object, ?[Node]) → ?Node
+  // Add a node at the current position.
+  _addNode(type, attrs, content) {
+    let node = type.createAndFill(attrs, content, this._marks)
+    if (!node) 
+      return null
+    if (this._stack.length) 
+      this._top().content.push(node)
+    return node
+  }
+
+  _maybeMerge(a, b) {
+    if (a.isText && b.isText && Mark.sameSet(a.marks, b.marks))
+      return a.withText(a.text + b.text)
   }
 }
 
-function tokenHandlers(tokenSpecs) {
-  let schema = pandocSchema;
-  let handlers = Object.create(null);
-  for (let type in tokenSpecs) {
-    let spec = tokenSpecs[type];
-    let getChildren = spec.getChildren || (tok => tok.c);
-    let getAttrs = spec.getAttrs || (() => {});
-    if (spec.text) {
-      handlers[type] = (state, tok) => {
-        let text = spec.getText(tok);
-        state.addText(text);
-      }
-    } else if (spec.mark) {
-      handlers[type] = (state, tok) => {
-        let markType = schema.marks[spec.mark];
-        let mark = markType.create(getAttrs(tok));
-        state.openMark(mark);
-        parseTokens(state, getChildren(tok), handlers);
-        state.closeMark(mark);
-      } 
-    } else if (spec.block) {
-      let nodeType = schema.nodeType(spec.block);
-      handlers[type] = (state, tok) => {
-        state.openNode(nodeType, getAttrs(tok));
-        parseTokens(state, getChildren(tok), handlers);
-        state.closeNode();
-      };
-    }
-  }
-  return handlers;
-}
+
 
